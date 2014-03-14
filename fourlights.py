68eb2d14a3abcca8709885f76b9f9e50323ac97a
Merlijn Wajer <merlijn@wizzup.org>
2014-03-14 20:13:17 +0100

    Triangular lights.

--- fourlights.py
+++ fourlights.py
@@ -9,17 +9,29 @@ import sys
 
 import ctypes
 
-RESOLUTION = 1024
+RESOLUTION = 1024 * 2
+SAMPLERATE = 44100
 
 from OpenGL.GLUT import *
 from OpenGL.GL import *
 
 import pyaudio
 
+def freq_to_fourier(hz):
+    return int(RESOLUTION * hz / SAMPLERATE)
+
+class FourLight(object):
+    def __init__(self, freqs):
+        self.freqs = freqs
+
+    def blinkenlights(self, freq):
+        pass
+
 class FourLights(object):
 
-    def __init__(self, inp):
+    def __init__(self, inp, lights):
         self.inp = inp
+        self.lights = lights
         self.freq = np.linspace(0.2, 0.8, RESOLUTION)
         self.doge = np.cos(np.linspace(0.0, (float(RESOLUTION - 1) / RESOLUTION) * 2 * pi, RESOLUTION))
 
@@ -87,7 +99,7 @@ class FourGL(object):
         self.vertices = []
         for i in xrange(RESOLUTION):
             p = float(i) / RESOLUTION
-            r = (1.0 / RESOLUTION) * 1.9 
+            r = (1.0 / RESOLUTION) * 1.9
             for _ in xrange(4):
                 cols.append((p, 0, 1 - p))
             x = p * 2 - 1
@@ -96,39 +108,98 @@ class FourGL(object):
             self.vertices.append((x + r, -1))
             self.vertices.append((x + r, p * 2 - 1))
 
-        cols = np.array(cols, dtype=np.float)
+        self.cols = np.array(cols, dtype=np.float)
         self.vertices = np.array(self.vertices, dtype=np.float)
         #glColorPointer(3, GL_FLOAT, 0, [0.,] * 9)
-        glColorPointer(3, GL_FLOAT, 0, cols)
+
+        # Create light pointers
+        self.pointer_verts = []
+        self.pointer_cols = []
+        for light in self.fl.lights:
+            print light.freqs
+            for freq, colour in zip(light.freqs,
+                    (np.identity(3) * 255.0).tolist()):
+                x = (float(freq_to_fourier(freq) + .5) / RESOLUTION) * 2 - 1
+                hs = 1.0 / RESOLUTION
+                #hs *= 5
+                self.pointer_verts.extend([
+                        (x - hs, 0),
+                        (x, -hs * 45),
+                        (x + hs, 0)
+                    ])
+                for _ in xrange(3):
+                    self.pointer_cols.append(colour)
+
+        self.pointer_cols = np.array(self.pointer_cols, dtype=np.float)
+        self.pointer_verts = np.array(self.pointer_verts, dtype=np.float)
+
+        print self.pointer_verts
 
     def draw(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(-1.0, -.8, -1.0, 1.0, -1.0, 1.0)
+        glMatrixMode(GL_MODELVIEW)
+
         glClear(GL_COLOR_BUFFER_BIT)
 
         self.vertices[::4, 1] = self.fl.freq * 2 - 1
         self.vertices[3::4, 1] = self.fl.freq * 2 - 1
+        glColorPointer(3, GL_FLOAT, 0, self.cols)
         glVertexPointer(2, GL_FLOAT, 0, self.vertices)
-
         glDrawArrays(GL_QUADS, 0, RESOLUTION * 4)
+
+        glColorPointer(3, GL_FLOAT, 0, self.pointer_cols)
+        glVertexPointer(2, GL_FLOAT, 0, self.pointer_verts)
+        glDrawArrays(GL_TRIANGLES, 0, len(self.fl.lights) * 3 * 3)
 
         glutSwapBuffers()
 
 
 GUI = True
-DMX = True
+DMX = False
 ECHO = False
 
 
 if __name__ == '__main__':
-    fl = FourLights(sys.stdin)
+    lampjes = []
+    for i in xrange(3):
+        lampjes.append(FourLight([SAMPLERATE / RESOLUTION * (j + (i * 3))
+            for j in xrange(3)]))
+    for tone in [0, 2, 3, 5, 7, 8, 10]:
+        lampjes.append(FourLight([(220.0 * (2.0 ** (octa + (tone/12.0))))
+            for octa in xrange(3)]))
+
+    fl = FourLights(sys.stdin, lampjes)
 
     if DMX:
         dmx = FourDMX()
         dmx.open()
 
-        def write_led(fourlights):
-            wr = [fourlights.freq[4 * (i + 1)] * 15 for i in xrange(12)]
+        def _write_led(fourlights):
+            avg = np.sum(fourlights.freq) / RESOLUTION * 2
+            wr = [fourlights.freq[4 * (i + 1)] * 45 for i in xrange(9)]
+            freq = np.array(fourlights.freq)
+            freq[freq < avg] = 0
+            freq[freq >= avg] -= avg
+            wr += [freq[freq_to_fourier(220.0 * (2.0 **
+                (oct + (tone/7.0))))] * 45 for tone in xrange(7)
+                for oct in xrange(3)][:21]
             dmx.write(wr)
 
+        def write_led(fourlights):
+            #avg = np.sum(fourlights.freq) / RESOLUTION * 2
+            #wr = [fourlights.freq[4 * (i + 1)] * 45 for i in xrange(9)]
+            #freq = np.array(fourlights.freq)
+            #freq[freq < avg] = 0
+            #freq[freq >= avg] -= avg
+            #wr += [freq[freq_to_fourier(220.0 * (2.0 **
+            #    (oct + (tone/7.0))))] * 45 for tone in xrange(7)
+            #    for oct in xrange(3)][:21]
+            wr = [fourlights.freq[freq_to_fourier(freq)] * 45
+                    for light in fl.lights
+                    for freq in light.freqs]
+            dmx.write(wr)
         fl.fcs.append(write_led)
 
     if ECHO:
