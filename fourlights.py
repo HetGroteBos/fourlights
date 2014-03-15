68f015b0af265cdf4aeeaf02eeda6e47ea2ccad5
Bas Weelinck <bas.weelinck@gmail.com>
2014-03-15 01:02:32 +0100

    LED-wall support.

--- fourlights.py
+++ fourlights.py
@@ -12,6 +12,9 @@ import ctypes
 RESOLUTION = 1024 * 2
 SAMPLERATE = 44100
 
+LEWDWALL_IP='10.0.20.16'
+LEWDWALL_PORT=8000
+
 from OpenGL.GLUT import *
 from OpenGL.GL import *
 
@@ -155,11 +158,42 @@ class FourGL(object):
 
         glutSwapBuffers()
 
+class FourLewds(object):
+    def __init__(self):
+        sys.path.insert(0, './lewd/src')
+        from remotescreen import RemoteScreen
+        self.w = 10
+        self.h = 12
+        self.screen = RemoteScreen(LEWDWALL_IP, LEWDWALL_PORT,
+            dimension=(self.w,self.h))
+
+        # hoi
+        f = [[(0, 0, 0) for y in range(self.h)] for x in range(self.w)]
+        self.screen.load_frame(f)
+        self.screen.push()
+
+    def write(self, freq):
+        fr = [freq[10 * (i + 1)] for i in xrange(12)]
+        f = [[(0, 0, 0) for y in range(self.h)] for x in range(self.w)]
+
+        def fill_bar(bar, fill):
+            for idx in range(fill):
+                f[9 - idx][bar] = (0, 30, 75)
+
+        for ind, frq in enumerate(fr):
+            #frq =frq*75
+            frq =frq*25
+            bars = int(round(min(frq, 10)))
+
+            fill_bar(ind, bars)
+
+        self.screen.load_frame(f)
+        self.screen.push()
 
 GUI = True
 DMX = False
 ECHO = False
-
+LEWD = False
 
 if __name__ == '__main__':
     lampjes = []
@@ -210,6 +244,14 @@ if __name__ == '__main__':
             au.write(w)
 
         fl.fcs.append(write_wav)
+
+    if LEWD:
+        lewd = FourLewds()
+
+        def write_lewd(fourlights):
+            lewd.write(fourlights.freq)
+
+        fl.fcs.append(write_lewd)
 
     if not GUI:
         while True:
