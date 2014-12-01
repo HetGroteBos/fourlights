ec5872e2d05ace70901e592be2c41b65971d9c60
Merlijn Wajer <merlijn@wizzup.org>
2014-12-02 00:50:48 +0100

    Initial stab at log()

--- four2d.py
+++ four2d.py
@@ -12,7 +12,7 @@ class Four2D(object):
         self.fl = fourlights
         self.g = glob
 
-        self.w = 1024 # 1920 / 2
+        self.w = 1920 # 1920 / 2
         self.h = 512
 
         self.screen = pygame.display.set_mode((self.w, self.h), 0, 32)
@@ -23,24 +23,52 @@ class Four2D(object):
 
         self.curr = 0
 
+        self.logarithmic = False
+
         pygame.init()
 
     def draw(self):
         left = self.fl.freql[:self.fl.window / 2][::-1] * 255
         right = self.fl.freqr[:self.fl.window / 2][::-1] * 255
 
-        trim = (self.fl.window / 2) / self.h
-        if trim != 1:
-            left = (left[::trim] + left[1::trim]) / trim
-            right = (right[::trim] + right[1::trim]) / trim
 
-        r = right.astype(np.int) << 16
-        g = left.astype(np.int) << 8
-        b = ((left + right) / 2).astype(np.int)
+        # TODO: Optimise this
+        if self.logarithmic:
+            # 0.90 is bad, but we don't want to go beyond the fourier index ...
+            # TODO
+            newpixelindex = np.log2(np.linspace(0., 0.90, self.h) * 32 + 1.0) / 5.
 
-        b = b | b << 8 | b << 16
+            # Scale up to self.fl.window / 2
+            newpixelindex *= self.fl.window / 2
+            newpixelindex = newpixelindex.astype(int)
 
-        self.pixels[self.curr,:] = r | g | b
+            lvals = left[newpixelindex]
+            rvals = right[newpixelindex]
+
+            r = rvals.astype(np.int) << 16
+            g = lvals.astype(np.int) << 8
+            b = ((lvals + rvals) / 2).astype(np.int)
+
+            b = b | b << 8 | b << 16
+
+            self.pixels[self.curr,:] = r | g | b
+
+            #self.pixels[self.curr,:] = left[newpixelindex]
+
+        # If not logarithmic view, apply trim & colouring
+        else:
+            trim = (self.fl.window / 2) / self.h
+            if trim != 1:
+                left = (left[::trim] + left[1::trim]) / trim
+                right = (right[::trim] + right[1::trim]) / trim
+
+            r = right.astype(np.int) << 16
+            g = left.astype(np.int) << 8
+            b = ((left + right) / 2).astype(np.int)
+
+            b = b | b << 8 | b << 16
+
+            self.pixels[self.curr,:] = r | g | b
 
         up = pygame.Rect(self.curr, 0, self.curr, self.h)
 
@@ -53,7 +81,15 @@ class Four2D(object):
         #pygame.display.flip()
 
     def run(self):
-        while not pygame.QUIT in [e.type for e in pygame.event.get()]:
+        while True:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    return
+
+                if e.type == pygame.KEYUP:
+                    if e.key == pygame.K_l:
+                        self.logarithmic = not self.logarithmic
+                        print 'Log switch'
 
             #t = time()
             self.fl.next()
